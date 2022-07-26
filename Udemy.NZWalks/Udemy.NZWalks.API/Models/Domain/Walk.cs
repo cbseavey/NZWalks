44892c124f03a4610de84b0f5c3d517a6a5ea6b2
CB Seavey <cbseavey@hotmail.com>
2022-07-25 23:17:18 -0500

    Section 02.25

---
+++ Udemy.NZWalks/Udemy.NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,15 @@
+﻿namespace Udemy.NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid WalkID { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalkDifficultyID { get; set; }
+
+        // Navigation Properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
---
+++ Udemy.NZWalks/Udemy.NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,8 @@
+﻿namespace Udemy.NZWalks.API.Models.Domain
+{
+    public class WalkDifficulty
+    {
+        public Guid WalkDifficultyID { get; set; }
+        public string Code { get; set; }
+    }
+}
